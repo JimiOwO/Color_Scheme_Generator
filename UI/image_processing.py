f95a77ce043f6b929440780ae6a5370294b3f56f
JimiOwO <jimipengowo@gmail.com>
2024-04-12 06:10:06 -0400

    ui

--- UI/image_processing.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from PIL import ImageTk
-
-def process_image(image):
-    # Example processing operation: Convert image to grayscale
-    processed_image = image.convert("L")
-    return ImageTk.PhotoImage(processed_image)
